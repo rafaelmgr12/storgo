262414907a099cb021ae2934cdf95e34e1d11e3c
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-08-11 16:34:28 -0300

    Add p2p package with Peer and Transport interfaces

---
+++ p2p/transport.go
@@ -0,0 +1,9 @@
+package p2p
+
+// Peer is an interface that represents the remote node
+type Peer interface{}
+
+// Tansport is anything that handlers the communication
+// between the nodes in the network. This can be of the
+// form (TCP,UDP websockets, ....)
+type Transport interface{}
