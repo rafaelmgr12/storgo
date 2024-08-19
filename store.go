77dd77f2fac343e7ea5a0e3d89fabe0a614eda7c
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-08-19 13:05:37 -0300

    feat: Implement writeStream function in Store

---
+++ store.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+type PathTransformFunc func(string) string
+
+type StoreOpts struct {
+	PathTransformFunc PathTransformFunc
+}
+
+// create a default transformation func
+
+var DefaultPathTansformFunc = func(key string) string {
+	return key
+}
+
+type Store struct {
+	StoreOpts
+}
+
+func NewStore(opts StoreOpts) *Store {
+	return &Store{
+		StoreOpts: opts,
+	}
+}
+
+func (s *Store) writeStream(key string, r io.Reader) error {
+	pathName := s.PathTransformFunc(key) //transformation of the key
+	// give the right privilige
+	if err := os.MkdirAll(pathName, os.ModePerm); err != nil {
+		return err
+	}
+
+	// TODO: Implement a hash
+	filename := "somefilename"
+
+	pathAndFilename := pathName + "/" + filename
+
+	f, err := os.Create(pathAndFilename)
+	if err != nil {
+		return err
+	}
+
+	n, err := io.Copy(f, r)
+
+	if err != nil {
+		return err
+	}
+
+	log.Printf("written (%d) bytes to disk: %s", n, pathAndFilename)
+
+	return nil
+}
