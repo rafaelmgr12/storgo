a339e52ad0a70a2c3a21031008874cad39eac527
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-08-19 20:30:00 -0300

    test: Add unit test for CASPathTansformFunc and fix typo in function name

--- store_test.go
+++ store_test.go
@@ -5,6 +5,17 @@ import (
 	"testing"
 )
 
+func TestPathTransformFunc(t *testing.T) {
+	key := "momsbestpicture"
+	pathname := CASPathTansformFunc(key)
+	expectedPathName := "68044/29f74/181a6/3c50c/3d81d/733a1/2f14a/353ff"
+	if pathname != expectedPathName {
+		t.Errorf("have %s want %s", pathname, expectedPathName)
+
+	}
+
+}
+
 func TestStore(t *testing.T) {
 	opts := StoreOpts{
 		PathTransformFunc: DefaultPathTansformFunc,
