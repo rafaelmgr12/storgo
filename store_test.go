3d2a90272fabfca99cc6ee60c8f64ba4de2be863
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-08-19 13:05:43 -0300

    test: Add test for writing to store

---
+++ store_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestStore(t *testing.T) {
+	opts := StoreOpts{
+		PathTransformFunc: DefaultPathTansformFunc,
+	}
+	s := NewStore(opts)
+
+	data := bytes.NewReader([]byte("some jpg bytes"))
+	if err := s.writeStream("myspecialpictures", data); err != nil {
+		t.Error(err)
+	}
+
+}
