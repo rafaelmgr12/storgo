f4a29c2b47d449eca67d5562dac15ef7d47ec98e
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-08-20 19:54:57 -0300

    Refactor: Improve Store's test coverage and delete unused code

--- store_test.go
+++ store_test.go
@@ -2,23 +2,10 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"testing"
 )
-
-func newStore() *Store {
-	opts := StoreOpts{
-		PathTransformFunc: CASPathTansformFunc,
-	}
-
-	return NewStore(opts)
-}
-
-func teardown(t *testing.T, s *Store) {
-	if err := s.Clear(); err != nil {
-		t.Error(err)
-	}
-}
 
 func TestPathTransformFunc(t *testing.T) {
 	key := "momsbestpicture"
@@ -37,48 +24,55 @@ func TestPathTransformFunc(t *testing.T) {
 
 }
 
-func TestStoreDeleteKey(t *testing.T) {
+func TestStore(t *testing.T) {
 	s := newStore()
 	defer teardown(t, s)
-	key := "momsspecials"
-	data := []byte("some jpg bytes")
-	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
-		t.Error(err)
-	}
+	for i := 0; i < 50; i++ {
+		key := fmt.Sprintf("foo_%d", i)
 
-	if err := s.Delete(key); err != nil {
+		data := []byte("some jpg bytes")
 
-		t.Error(err)
+		if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
+			t.Error(err)
+		}
 
+		if ok := s.Has(key); !ok {
+			t.Errorf("expetected to have key %s", key)
+		}
+
+		r, err := s.Read(key)
+
+		if err != nil {
+			t.Error(err)
+		}
+
+		b, _ := io.ReadAll(r)
+
+		if string(b) != string(data) {
+			t.Errorf("want %s have %s", data, b)
+		}
+
+		if err := s.Delete(key); err != nil {
+			t.Error(err)
+		}
+
+		if ok := s.Has(key); ok {
+			t.Errorf("expected to NOT have key %s", key)
+		}
 	}
 
 }
 
-func TestStore(t *testing.T) {
-	s := newStore()
-	defer teardown(t, s)
-	key := "momsspecials"
-	data := []byte("some jpg bytes")
-	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
+func newStore() *Store {
+	opts := StoreOpts{
+		PathTransformFunc: CASPathTansformFunc,
+	}
+
+	return NewStore(opts)
+}
+
+func teardown(t *testing.T, s *Store) {
+	if err := s.Clear(); err != nil {
 		t.Error(err)
 	}
-
-	if ok := s.Has(key); !ok {
-		t.Errorf("expetected to have key %s", key)
-	}
-
-	r, err := s.Read(key)
-
-	if err != nil {
-		t.Error(err)
-	}
-
-	b, _ := io.ReadAll(r)
-
-	if string(b) != string(data) {
-		t.Errorf("want %s have %s", data, b)
-	}
-
-	s.Delete(key)
-
 }
