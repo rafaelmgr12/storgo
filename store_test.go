a6b2696d4f65365a8d6cb8178634112bbdb2fa26
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-08-20 10:22:14 -0300

    test: fix store test

--- store_test.go
+++ store_test.go
@@ -8,9 +8,15 @@ import (
 func TestPathTransformFunc(t *testing.T) {
 	key := "momsbestpicture"
 	pathname := CASPathTansformFunc(key)
+	expectedOriginalKey := "6804429f74181a63c50c3d81d733a12f14a353ff"
 	expectedPathName := "68044/29f74/181a6/3c50c/3d81d/733a1/2f14a/353ff"
-	if pathname != expectedPathName {
-		t.Errorf("have %s want %s", pathname, expectedPathName)
+	if pathname.PathName != expectedPathName {
+		t.Errorf("have %s want %s", pathname.PathName, expectedPathName)
+
+	}
+
+	if pathname.Original != expectedOriginalKey {
+		t.Errorf("have %s want %s", pathname.Original, expectedOriginalKey)
 
 	}
 
