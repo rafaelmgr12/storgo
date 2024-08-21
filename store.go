2a757b76df0010b8ee948697bacc5538a4f5c04b
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-08-21 13:02:07 -0300

    Refactor: Add Write method to Store for writing data to the store

--- store.go
+++ store.go
@@ -101,6 +101,10 @@ func (s *Store) Clear() error {
 	return os.RemoveAll(s.Root)
 }
 
+func (s *Store) Write(key string, r io.Reader) error {
+	return s.writeStream(key, r)
+}
+
 func (s *Store) Delete(key string) error {
 	pathKey := s.PathTransformFunc(key)
 
