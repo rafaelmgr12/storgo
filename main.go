4b7810ea025f91bc3118f153e6c7f9b91da2212c
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-08-22 18:30:56 -0300

    feat: Update main.go to support multiple servers with different listen addresses

--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"log"
-	"time"
 
 	"github.com/rafaelmgr12/storgo/p2p"
 )
 
-func main() {
+func makeServer(listenAddr string, nodes ...string) *FileServer {
 	tcpTransportOpts := p2p.TCPTransportOpts{
-		ListenAddr:    ":3000",
+		ListenAddr:    listenAddr,
 		HandshakeFunc: p2p.NOPHandshakeFunc,
 		Decoder:       p2p.DefaultDecoder{},
 		// TODO: onPeer func
@@ -17,20 +16,23 @@ func main() {
 	tcpTransport := p2p.NewTCPTransport(tcpTransportOpts)
 
 	fileServerOpts := FileServerOpts{
-		ListenAddr:        ":3000",
-		StorageRoot:       "3000_network",
+		StorageRoot:       listenAddr + "_network",
 		PathTransformFunc: CASPathTansformFunc,
 		Transport:         tcpTransport,
+		BootstrapNodes:    nodes,
 	}
 
-	fs := NewFileServer(fileServerOpts)
+	return NewFileServer(fileServerOpts)
+}
+
+func main() {
+
+	s1 := makeServer(":3000", "")
+	s2 := makeServer(":4000", ":3000")
+
 	go func() {
-		time.Sleep(time.Second * 3)
-		fs.Stop()
+		log.Fatal(s1.Start())
 	}()
 
-	if err := fs.Start(); err != nil {
-		log.Fatal(err)
-	}
-
+	s2.Start()
 }
