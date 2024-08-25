e96c63af48d6a35970d1248a068f26daa437623c
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-08-25 18:47:44 -0300

    Refactor: Update TCPPeer to use embedded net.Conn for underlying connection

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"sync"
 )
 
 // TCPPeer represents the remote node over a TCP established connection
@@ -15,12 +16,15 @@ type TCPPeer struct {
 	// outbound is true if we dial and retrieve a connection
 	// outbound is false if we accept and retrieve a connection
 	outbound bool
+
+	Wg *sync.WaitGroup
 }
 
 func NewTCPPeer(conn net.Conn, outbound bool) *TCPPeer {
 	return &TCPPeer{
 		Conn:     conn,
 		outbound: outbound,
+		Wg:       &sync.WaitGroup{},
 	}
 }
 
@@ -138,9 +142,14 @@ func (t *TCPTransport) handleConn(conn net.Conn, outbound bool) {
 			return
 		}
 
-		rpc.From = conn.RemoteAddr()
+		rpc.From = conn.RemoteAddr().String()
+		peer.Wg.Add(1)
+		fmt.Println("waiting till stream is done")
 
 		t.rpcch <- rpc
+
+		peer.Wg.Wait()
+		fmt.Println("stream done continueing normal read loop")
 	}
 
 }
