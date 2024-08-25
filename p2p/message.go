7ebcedac919a08c91416c89faa2319b9416adec6
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-08-25 18:47:50 -0300

    Refactor: Update RPC struct to use string instead of net.Addr for 'From' field

--- p2p/message.go
+++ p2p/message.go
@@ -1,10 +1,8 @@
 package p2p
 
-import "net"
-
 // RPC represents any arbitraty data that is being sent over the
 // each transport bewteen two nodes in the network
 type RPC struct {
-	From    net.Addr
+	From    string
 	Payload []byte
 }
