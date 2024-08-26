84458ea5806b1e538b2b3d9a195edc07b270d64d
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2024-08-26 17:39:46 -0300

    Refactor: Update FileServer.StoreData to include file size in MessageStoreFile payload

--- server.go
+++ server.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"sync"
+	"time"
 
 	"github.com/rafaelmgr12/storgo/p2p"
 )
@@ -53,7 +54,8 @@ type Message struct {
 }
 
 type MessageStoreFile struct {
-	Key string
+	Key  string
+	Size int64
 }
 
 func (s *FileServer) broadcast(msg *Message) error {
@@ -68,58 +70,49 @@ func (s *FileServer) broadcast(msg *Message) error {
 }
 
 func (s *FileServer) StoreData(key string, r io.Reader) error {
-	// 1. Store this file to disk
-	// 2. broadcast this file to all know peers in the network
 
 	buf := new(bytes.Buffer)
+	tee := io.TeeReader(r, buf)
+
+	size, err := s.store.Write(key, tee)
+	if err != nil {
+		return err
+	}
+
 	msg := Message{
 		Payload: MessageStoreFile{
-			Key: key,
+			Key:  key,
+			Size: size,
 		},
 	}
 
-	if err := gob.NewEncoder(buf).Encode(msg); err != nil {
+	msgBuf := new(bytes.Buffer)
+
+	if err := gob.NewEncoder(msgBuf).Encode(msg); err != nil {
 		return err
 	}
 
 	for _, peer := range s.peers {
-		if err := peer.Send(buf.Bytes()); err != nil {
+		if err := peer.Send(msgBuf.Bytes()); err != nil {
 			return err
 		}
 
 	}
 
-	// time.Sleep(3 * time.Second)
+	time.Sleep(3 * time.Second)
 
-	// payload := []byte("THIS IS A LARGE FILE")
-	// for _, peer := range s.peers {
-	// 	if err := peer.Send(payload); err != nil {
-	// 		return err
-	// 	}
+	for _, peer := range s.peers {
+		n, err := io.Copy(peer, buf)
+		if err != nil {
+			return err
+		}
 
-	// }
+		fmt.Println("recevied and written bytes to disk: ", n)
+
+	}
 
 	return nil
 
-	// buf := new(bytes.Buffer)
-
-	// tee := io.TeeReader(r, buf)
-
-	// if err := s.store.Write(key, tee); err != nil {
-	// 	return err
-	// }
-
-	// p := &DataMessage{
-	// 	Key:  key,
-	// 	Data: buf.Bytes(),
-	// }
-
-	// fmt.Println(buf.Bytes())
-
-	// return s.broadcast(&Message{
-	// 	From:    "TODO", // TODO: Implement a way to obtain the address of the sender
-	// 	Payload: p,
-	// })
 }
 
 func (s *FileServer) OnPeer(p p2p.Peer) error {
@@ -148,25 +141,9 @@ func (s *FileServer) loop() {
 				log.Println(err)
 			}
 
-			fmt.Printf("%+v\n", msg.Payload)
-
-			peer, ok := s.peers[rpc.From]
-			if !ok {
-				panic("peer not found in peer map")
+			if err := s.handleMessage(rpc.From, &msg); err != nil {
+				log.Println(err)
 			}
-
-			b := make([]byte, 1000)
-			if _, err := peer.Read(b); err != nil {
-				panic(err)
-			}
-
-			fmt.Printf("%s\n", string(b))
-
-			peer.(*p2p.TCPPeer).Wg.Done() // it is not correct .....
-
-			// if err := s.handleMessage(&m); err != nil {
-			// 	log.Println(err)
-			// }
 
 		case <-s.quitch:
 			return
@@ -174,17 +151,32 @@ func (s *FileServer) loop() {
 	}
 }
 
-// func (s *FileServer) handleMessage(msg *Message) error {
+func (s *FileServer) handleMessage(from string, msg *Message) error {
 
-// 	switch v := msg.Payload.(type) {
-// 	case *DataMessage:
-// 		fmt.Printf("recevied data %+v\n", v)
+	switch v := msg.Payload.(type) {
+	case MessageStoreFile:
+		return s.handleMessageStoreFile(from, v)
 
-// 	}
+	}
 
-// 	return nil
-// }
+	return nil
+}
 
+func (s *FileServer) handleMessageStoreFile(from string, msg MessageStoreFile) error {
+
+	peer, ok := s.peers[from]
+	if !ok {
+		return fmt.Errorf("peer (%s) could not be found in the peer list", from)
+	}
+
+	if _, err := s.store.Write(msg.Key, io.LimitReader(peer, msg.Size)); err != nil {
+		return err
+	}
+
+	peer.(*p2p.TCPPeer).Wg.Done()
+
+	return nil
+}
 func (s *FileServer) bootstrapNetwork() error {
 	for _, addr := range s.BootstrapNodes {
 		if len(addr) == 0 {
